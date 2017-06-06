cde9fea8b1fe86e6d1241a21c0e8d682b0fd2c98
Scott James Remnant <scott@netsplit.com>
2017-06-06 11:39:04 -0700

    Pull signal analysis out of ISR
    Pretty much a total rework of the detector code, moving everything
    aside from edge delta calculation out of the ISR and into the main
    loop.
    
    Rather than a ring-buffer (which overflowed when the UART was too
    slow to keep up), just check for edges at the end of the loop. If
    they occur faster than we can process, we either have a bad signal
    or a problem.

--- AVR/Detector/detector.c
+++ AVR/Detector/detector.c
@@ -1,42 +1,75 @@
+//
+//  detector.c
+//  SignalBox
+//
+//  Created by Scott James Remnant on 06/05/17.
+//
+//
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-
-#include <util/delay.h>
 
 #include <stdio.h>
 #include <string.h>
 
-#if WITH_UART
 #include "uart.h"
-#endif
 
+
+// Input Signal Timing
+// -------------------
+// The DCC signal is a a series of high/low periods where the length of the
+// high and low parts indicate whether it's a zero-bit or a one-bit. Either
+// the high or low can come first, depending on the wiring:
+//
+//    __    __   _   _        __    __    _   _
+// __|  |__|  |_| |_| |  or  |  |__|  |__| |_| |_  =>  0011
+//
+// We use a pair of interrupts to measure the time, in microseconds, between
+// edges. TIMER0 counts the number of 4µs ticks in TCNT0, and the ISR counts
+// the number of times it overflows; thus we can combine the two values to
+// calculate the number of microseconds that have passed.
+//
+// INT0 is fired on the edges, and makes this calculation, comparing it to the
+// previous edge time, and storing the result in `delta`. The `edge` flag is
+// set on each new edge.
+
+static inline void init() {
+	// To analyze the DCC signal we need a timer on which we can count,
+	// with reasonable precision, microseconds. Set up TIMER0 for 4µs
+	// ticks (64 prescale) which is reasonable enough.
+	TCCR0A = _BV(WGM01) | _BV(WGM00);
+	TCCR0B = _BV(CS01) | _BV(CS00);
+	TIMSK0 = _BV(TOIE0);
+
+	// Configure D2 (INT0) for input, disable the pull-up.
+	DDRD &= ~_BV(DDD2);
+	PORTD &= ~_BV(PORTD2);
+
+	// Configure INT0 to generate an interrupt for any logical change.
+	EICRA |= _BV(ISC00);
+	EIMSK |= _BV(INT0);
+} 
 
 volatile unsigned long timer0_ovf_count;
 
 // TIMER0 Overflow Interrupt.
 //
-// Keep count of overflows so that we can calculate deltas larger than
-// 1.02ms.
+// Keep count of overflows, store in `timer0_ovf_count`.
 ISR(TIMER0_OVF_vect)
 {
 	++timer0_ovf_count;
 }
 
-#define BMAX 8192
-
-volatile unsigned long last_micros, last_delta;
-volatile int last_bit = -1, phase = -1, bpos, invalid;
-volatile uint8_t buffer[1024];
+volatile unsigned long last_micros, delta;
+volatile uint8_t edge;
 
 // INT0 Interrupt.
 // Fires when the input signal on INT0 (D2) changes.
 //
-// Tracks the deltas between changes, to determine whether we just saw a valid
-// 0-bit or 1-bit and pushes the value into the bit buffer for the main loop
-// to process.
+// Tracks the deltas between edges using TIMER0.
 ISR(INT0_vect)
 {
-	unsigned long ovf, micros, delta;
+	unsigned long ovf, micros;
 	uint8_t tcnt;
 
 	ovf = timer0_ovf_count;
@@ -48,78 +81,48 @@ ISR(INT0_vect)
 
 	micros = (ovf << 10) | (tcnt << 2);
 	delta = micros - last_micros;
+	edge = 1;
 	last_micros = micros;
-
-	// The specification says to allow 52-64µs for a one-bit, and 90-10,000µs
-	// for a zero; since our timer resolution is only 4µs we allow +/- that
-	// on top.
-	int bit;
-	if (delta >= 48 && delta <= 68) {
-		bit = 1;
-	} else if (delta >= 84 && delta <= 10004) {
-		bit = 0;
-	} else {
-		// An invalid bit means we bail out and reset the state.
-		invalid |= 1;
-		last_bit = -1;
-		phase = -1;
-		return;
-	}
-
-	switch (phase) {
-	case -1:
-		// Initial phase requires that we see a transition between one and zero
-		// before we can start processing bits.
-		if (last_bit != -1 && last_bit != bit) {
-			phase = 0;
-		} else {
-			last_bit = bit;
-			break;
-		}
-		// Possibly fall-through from above.
-	case 0:
-		// Save the bit, delta, and move to the next phase in the next cycle.
-		last_bit = bit;
-		last_delta = delta;
-		phase = 1;
-		return;
-	case 1:
-		// Bits must match, otherwise we bail.
-		if (last_bit != bit) {
-			invalid |= 2;
-			last_bit = -1;
-			phase = -1;
-			return;
-		}
-
-		// Double-check the delta of 1s, and treat as an invalid bit if we go out of spec.
-		if (bit) {
-			unsigned long diff = last_delta > delta ? last_delta - delta : delta - last_delta;
-			if (diff > 8) {
-				invalid |= 4;
-				last_bit = -1;
-				phase = -1;
-				return;
-			}
-		}
-
-		// Always move back to phase 0 if the bit was valid.
-		// We don't need last_bit or last_delta in phase 0.
-		phase = 0;
-		break;
-	}
-
-	// Bit is valid at this point.
-	if (bit) {
-		buffer[bpos / 8] |= _BV(bpos % 8);
-	} else {
-		buffer[bpos / 8] &= ~_BV(bpos % 8);
-	}
-
-	bpos++;
-	bpos %= BMAX;
 }
 
+
+// Main Loop
+// ---------
+// There are three basic passes to analyzing the incoming DCC signal.
+//
+// The first is to determine the phase. When we start to receive incoming
+// bits, we do not yet know where the boundary between each bits high and low
+// part occurs; a stream of zero-bits or one-bits looks the same when you're
+// in phase and out of phase. To synchronize we look for the point at which
+// the period length changes:
+//
+//                   :
+//   _   _   _   _    __    __   _   _ 
+// _| |_| |_| |_| |__|  |__|  |_| |_| |
+//                   :
+//         length change means we
+//         must be now in Phase A
+//
+// The second pass is packet and byte extraction. Initially that means locating
+// a preamble, ignoring bits until we find a sequence of one-bits of the right
+// length. Once found, the packet structure can then be followed logically and
+// byte and packet boundaries followed provided the input conforms to
+// specification.
+//
+//    ?    preamble.   byte     byte.    byte
+//        +--------+ +------+ +------+ +------+
+// 1101000111111111101010101001111000000101101011111...
+//                  :        :        :        :
+//                  +--byte end bits--+    packet end bit
+//
+// The third and final pass is taking the bytes within the packet and
+// determining the DCC instructions contained within.
+
+#define DELTA(_a, _b) ((_a) > (_b) ? (_a) - (_b) : (_b) - (_a))
+
+#define UNKNOWN 0
+#define A 1
+#define B 2
 
 #define SEEKING_PREAMBLE 0
 #define PREAMBLE 1
@@ -128,107 +131,173 @@ ISR(INT0_vect)
 int main()
 {
 	cli();
-
-	// To analyze the DCC signal we need a timer on which we can count,
-	// with reasonable precision, microseconds. Set up TIMER0 for 4µs
-	// ticks (64 prescale) which is reasonable enough.
-	TCCR0A = _BV(WGM01) | _BV(WGM00);
-	TCCR0B = _BV(CS01) | _BV(CS00);
-	TIMSK0 = _BV(TOIE0);
-
-	// Configure B5 for output, and set.
-	DDRB |= _BV(DDB5);
-	PORTB |= _BV(PORTB5);
-
-	// Configure D2 (INT0) for input, disable the pull-up.
-	DDRD &= ~_BV(DDD2);
-	PORTD &= ~_BV(PORTD2);
-
-	// Configure INT0 to generate an interrupt for any logical change.
-	EICRA |= _BV(ISC00);
-	EIMSK |= _BV(INT0);
-
+	init();
 	sei();
 
-#if WITH_UART
-	uart_init(UART_BAUD_SELECT(9600, F_CPU));
+	uart_init(UART_BAUD_SELECT(115200, F_CPU));
 	uart_puts("Running\r\n");
-#endif
 
-	int bstart = 0, state = SEEKING_PREAMBLE, preamble_bits = 0, b = 0;
-	uint8_t byte;
+	unsigned long last_length = 0;
+	int phase = UNKNOWN, last_bit = -1;
+	int state, preamble_length, bit_num;
+	uint8_t byte, check_byte;
 	for (;;) {
-		uint8_t _SREG = SREG;
-		int btop, is_invalid;
+		// Wait for an edge from the input ISR, and copy its length.
+		unsigned long length;
+		if (!edge) continue;
 
-		// We only guard access to btop, and otherwise use the ring buffer
-		// with interrupts still running. It takes over 0.8s to fill, so this
-		// should be safe as long as the uint8_t access is atomic over the bit
-		// set/clear.
 		cli();
-		btop = bpos;
-		is_invalid = invalid;
-		invalid = 0;
-		SREG = _SREG;
+		length = delta;
+		edge = 0;
+		sei();
 
-		if (is_invalid) {
+		// The specification says to allow 52-64µs for a one-bit, and
+		// 90-10,000µs for a zero-bit; since our timer resolution is only 4µs
+		// we allow +/- that on top.
+		int bit;
+		if (length >= 48 && length <= 68) {
+			bit = 1;
+		} else if (length >= 84 && length <= 10004) {
+			bit = 0;
+		} else {
+			// On an invalid bit length, attempt to resynchronize the phase.
 			char line[80];
-			sprintf(line, "Invalid %x\r\n", is_invalid);
+			sprintf(line, "Invalid bit length: %lu\r\n", length);
 			uart_puts(line);
-			_delay_ms(5000);
-			continue;
+
+			phase = UNKNOWN;
+			last_bit = -1;
+			goto next_edge;
 		}
 
-		for (int i = bstart; i != btop; i = (i + 1) % BMAX) {
-			int bit = buffer[i / 8] & _BV(i % 8);
+		// Each bit has two phases; we first need to train ourselves where
+		// the phase-change occurs, and then consume the bit in each phase.
+		switch (phase) {
+		case UNKNOWN:
+			// Wait for a transition between bit lengths before we start
+			// processing bits.
+			if (last_bit != -1 && last_bit != bit) {
+				phase = A;
 
-			switch (state) {
-			case SEEKING_PREAMBLE:
-				if (bit) {
-					++preamble_bits;
-				} else if (preamble_bits >= 10) {
-					// End of preamble found.
-					state = PACKET;
+				state = SEEKING_PREAMBLE;
+				preamble_length = 0;
+			} else {
+				last_bit = bit;
+				goto next_edge;
+			}
+			// Possibly fall-through.
+		case A:
+			// Save the bit, length, and move to the next phase in next cycle.
+			last_bit = bit;
+			last_length = length;
+			phase = B;
+			goto next_edge;
+		case B:
+			// Bits must match between phases; if they don't, we've probably
+			// gone out of phase, so resynchronize again.
+			if (last_bit != bit) {
+				char line[80];
+				sprintf(line, "Bit length didn't match in B: %d != %d\r\n", bit, last_bit);
+				uart_puts(line);
 
-					char line[80];
-					sprintf(line, "Preamble of %d bits\r\n", preamble_bits);
-					uart_puts(line);
-				} else {
-					preamble_bits = 0;
-				}
-				break;
-			case PREAMBLE:
-				if (!bit) {
-					state = PACKET;
-					uart_puts("\r\n");
-				}
-				break;	
-			case PACKET:
-				if (b < 8) {
-					if (bit) {
-						byte |= _BV(7 - b);
-					} else {
-						byte &= ~_BV(7 - b);
-					}
-					++b;
-				} else {
-					// byte contains a valid byte.
-					char line[80];
-					for (int j = 0; j < 8; ++j)
-						line[j] = byte & _BV(7 - j) ? '1' : '0';
-					line[8] = ' ';
-					line[9] = '\0';
-					uart_puts(line);
-
-					b = 0;
-					if (bit)
-						state = PREAMBLE;
-				}
-				break;
+				phase = UNKNOWN;
+				last_bit = -1;
+				goto next_edge;
 			}
 
+			// Double-check the delta of one-bit phases, if we go out of spec,
+			// treat it the same as if we had non-matching bits and
+			// resynchronize the phase.
+			if (bit && DELTA(length, last_length) > 8) {
+				char line[80];
+				sprintf(line, "Invalid delta: %lu - %lu\r\n", length, last_length);
+				uart_puts(line);
+
+				phase = UNKNOWN;
+				last_bit = -1;
+				goto next_edge;
+			}
+
+			// Back to phase A in next cycle, we won't need the last_bit or
+			// last_length for that.
+			phase = A;
+			break;
 		}
 
-		bstart = btop;
+		// Once we reach here, `bit` contains a valid bit after both phases
+		// have been seen. We can now locate the packet boundaries and perform
+		// packet extraction.
+		switch (state) {
+		case SEEKING_PREAMBLE:
+			// When we're looking for a preamble, we're looking for the first
+			// stretch of at lest 10 one-bits.
+			if (bit) {
+				++preamble_length;
+			} else if (preamble_length >= 10) {
+				// End of preamble found.
+				state = PACKET;
+				bit_num = 0;
+				check_byte = 0;
+			} else {
+				preamble_length = 0;
+			}
+			goto next_edge;
+		case PREAMBLE:
+			// In subsequent packets, we know where we are so we just handle
+			// the preamble train as a check.
+			if (!bit) {
+				state = PACKET;
+				bit_num = 0;
+				check_byte = 0;
+			}
+			goto next_edge;
+		case PACKET:
+			// Within the packet there are eight bits to a byte, followed by
+			// a zero-bit or a one-bit that determines whether more bytes
+			// follow, or a preamble.
+			if (bit_num < 8) {
+				if (bit) {
+					byte |= _BV(7 - bit_num);
+				} else {
+					byte &= ~_BV(7 - bit_num);
+				}
+				++bit_num;
+
+				uart_putc(bit ? '1' : '0');
+				goto next_edge;
+			} else {
+				if (bit)
+					state = PREAMBLE;
+				else
+					bit_num = 0;
+			}
+			break;
+		}
+
+		// Once we reach here, we have a full `byte` that we can analyze. When
+		// `bit` is one, this is the final byte in the instruction and is
+		// checked for errors.
+		if (bit) {
+			// Last byte in the instruction is always the error check byte.
+			if (byte != check_byte) {
+				uart_puts(" ERR\r\n");
+			} else {
+				uart_puts(" OK\r\n");
+			}
+		} else {
+			check_byte ^= byte;
+			uart_putc(' ');
+		}
+
+next_edge:
+		// Make sure no edge has occurred while we were processing.
+		if (edge) {
+			char line[80];
+			sprintf(line, "Edge occurred during processing\r\n");
+			uart_puts(line);
+
+			phase = UNKNOWN;
+			last_bit = -1;
+		}
 	}
 }
