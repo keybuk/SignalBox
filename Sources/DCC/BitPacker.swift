1060952e0e296528e593915317edca880838e8ba
Scott James Remnant <scott@netsplit.com>
2018-05-15 20:49:38 -0700

    Serial API for BitPacker
    In hindsight specifying all of the bit offsets is probably an unnecessary
    chore, so add a simpler version of the API without it:
    
      func add(into packer: inout BitPacker) {
          packer.add(value, length: 4)
          packer.add(otherValue, length: 2)
          packer.add(boolValue)
      }
    
    One cute thing about this is that I can use it for both packing the bytes
    of a packet, and then inserting the 0-bits between them.
    
      for byte in bytes {
        packer.add(byte, length: 8)
        packer.add(0, length: 1)
      }
    
      let errD = bytes.reduce(0, { $0 ^ $1 })
      packer.add(errD, length: 8)
      packer.add(1, length: 1)

--- Sources/DCC/BitPacker.swift
+++ Sources/DCC/BitPacker.swift
@@ -23,8 +23,9 @@ public protocol BitPackable {
 /// Packs sequences of bits into an array of bytes.
 ///
 /// `BitPacker` may be used to create arrays of bytes by sourcing values of individual bits from
-/// integers and values, packing their bits into the appropriate offsets within the larger
-/// structure.
+/// integers and values, packing their bits into the larger structure.
+///
+/// Values can be added serially, or at fields with specific positions within a byte.
 public struct BitPacker : CustomDebugStringConvertible {
     
     /// Packed bytes.
@@ -42,7 +43,39 @@ public struct BitPacker : CustomDebugStringConvertible {
         return "<BitPacker \(bitsString), remaining: \(bitsRemaining)>"
     }
     
-    /// Adds contents of a value to a bit field.
+    /// Add a bit field with the contents of a value.
+    ///
+    /// The length of the bit field is given in `length`. Only the lowest `length` bits from
+    /// `value` are used for the contents of the field.
+    ///
+    /// A new byte is added whenever necessary to contain the length of all fields. Fields may
+    /// span byte boundaries, and may be multiple bytes in length.
+    ///
+    /// - parameters:
+    ///   - value: value to add.
+    ///   - length: length of the field.
+    public mutating func add<T>(_ value: T, length: Int) where T : FixedWidthInteger {
+        assert(length > 0, "length must be greater than 0")
+        assert(length <= T.bitWidth, "length must be less than \(T.bitWidth)")
+        
+        var length = length
+        repeat {
+            if bitsRemaining < 1 {
+                bytes.append(0)
+                bitsRemaining = UInt8.bitWidth
+            }
+            
+            let chunkLength = min(length, bitsRemaining)
+            bitsRemaining -= chunkLength
+            length -= chunkLength
+            
+            let mask: T = ~(~0 << chunkLength)
+            let bits = UInt8(truncatingIfNeeded: (value >> length) & mask)
+            bytes[bytes.index(before: bytes.endIndex)] |= bits << bitsRemaining
+        } while length > 0
+    }
+
+    /// Add a bit field with the contents of a value.
     ///
     /// Fields are described by a position and a length; the position is given in `at` and gives
     /// the bit number of the most signifiant bit of the field, with a range of 0 to 7 inclusive.
@@ -51,7 +84,7 @@ public struct BitPacker : CustomDebugStringConvertible {
     /// `value` are used for the contents of the field.
     ///
     /// A new byte is added when a new bit field is added which would overlap any previously
-    /// added bit field.
+    /// added bit field, or when necessary to contain the length of all fields.
     ///
     /// Fields may span byte boundaries, and may be multiple bytes in length.
     ///
@@ -60,6 +93,7 @@ public struct BitPacker : CustomDebugStringConvertible {
     ///   - at: bit position where the field starts, range 0...7
     ///   - length: length of the field.
     public mutating func add<T>(_ value: T, at: Int, length: Int) where T : FixedWidthInteger {
+        // FIXME: I'm not sure this will be necessary
         assert(at < 8, "at must be in range 0..<8")
         assert(length > 0, "length must be greater than 0")
         assert(length <= value.bitWidth, "length must be less than \(value.bitWidth)")
@@ -81,23 +115,45 @@ public struct BitPacker : CustomDebugStringConvertible {
         } while length > 0
     }
     
+    /// Add a single bit field with the given value.
+    ///
+    /// A new byte is added whenever necessary to contain the length of all fields.
+    ///
+    /// - parameters:
+    ///   - value: whether to set the bit to 1 or 0.
+    public mutating func add(_ value: Bool) {
+        if bitsRemaining < 1 {
+            bytes.append(0)
+            bitsRemaining = UInt8.bitWidth
+        }
+
+        bitsRemaining -= 1
+        
+        if value {
+            bytes[bytes.index(before: bytes.endIndex)] |= 1 << bitsRemaining
+        }
+    }
+    
     /// Set the value of an individual bit in a bit field.
+    ///
+    /// A new byte is added whenever necessary to contain the length of all fields.
     ///
     /// - parameters:
     ///   - value: whether to set the bit to 1 or 0.
     ///   - at: bit position of the field, range 0...7.
-    public mutating func set(_ value: Bool, at: Int) {
+    public mutating func add(_ value: Bool, at: Int) {
+        // FIXME: I'm not sure this will be necessary
         assert(at < 8, "at must be in range 0..<8")
 
         if at >= bitsRemaining {
             bytes.append(0)
         }
 
-        if value {
-            bytes[bytes.index(before: bytes.endIndex)] |= 1 << at
-        }
-
         bitsRemaining = at
+
+        if value {
+            bytes[bytes.index(before: bytes.endIndex)] |= 1 << bitsRemaining
+        }
     }
     
 }
--- Tests/DCCTests/BitPackerTests.swift
+++ Tests/DCCTests/BitPackerTests.swift
@@ -12,6 +12,14 @@ import DCC
 
 class BitPackerTests: XCTestCase {
     
+    /// Add a value.
+    func testAdd() {
+        var packer = BitPacker()
+        packer.add(0b1111, length: 4)
+        
+        XCTAssertEqual(packer.bytes, [ 0b11110000 ])
+    }
+    
     /// Add a value at the top of a byte.
     func testStartOfByte() {
         var packer = BitPacker()
@@ -36,8 +44,17 @@ class BitPackerTests: XCTestCase {
         XCTAssertEqual(packer.bytes, [ 0b00001111 ])
     }
     
-    /// Add consecutive values within a byte.
+    /// Add multiple values.
     func testMultipleValues() {
+        var packer = BitPacker()
+        packer.add(0b1010, length: 4)
+        packer.add(0b1111, length: 4)
+        
+        XCTAssertEqual(packer.bytes, [ 0b10101111 ])
+    }
+
+    /// Add consecutive values within a byte.
+    func testConsecutiveValues() {
         var packer = BitPacker()
         packer.add(0b1010, at: 7, length: 4)
         packer.add(0b1111, at: 3, length: 4)
@@ -66,24 +83,67 @@ class BitPackerTests: XCTestCase {
     /// Adding a value can extend into a new byte.
     func testExtendingValue() {
         var packer = BitPacker()
+        packer.add(0b10, length: 2)
+        packer.add(0b11111111, length: 8)
+        
+        XCTAssertEqual(packer.bytes, [ 0b10111111, 0b11000000 ])
+    }
+    
+    /// Adding a value with an offset can extend into a new byte.
+    func testExtendingConsecutiveValue() {
+        var packer = BitPacker()
         packer.add(0b10, at: 7, length: 2)
         packer.add(0b11111111, at: 5, length: 8)
         
         XCTAssertEqual(packer.bytes, [ 0b10111111, 0b11000000 ])
     }
-    
+
     /// Add a value that is longer than a byte.
     func testLongValue() {
         var packer = BitPacker()
-        packer.add(0b11000011_11000011, at: 7, length: 16)
+        packer.add(0b11000011_11000011, length: 16)
         
         XCTAssertEqual(packer.bytes, [ 0b011000011, 0b11000011 ])
     }
     
-    /// Set a bit to true.
-    func testSet() {
+    /// Add a value that is longer than a byte with an offset.
+    func testLongValueAtOffset() {
         var packer = BitPacker()
-        packer.set(true, at: 7)
+        packer.add(0b11000011_11000011, at: 5, length: 16)
+        
+        XCTAssertEqual(packer.bytes, [ 0b000110000, 0b11110000, 0b11000000 ])
+    }
+    
+
+    /// Add a single bit field with true.
+    func testAddTrue() {
+        var packer = BitPacker()
+        packer.add(true)
+        
+        XCTAssertEqual(packer.bytes, [ 0b10000000 ])
+    }
+    
+    /// Add a single bit field with false.
+    func testAddFalse() {
+        var packer = BitPacker()
+        packer.add(false)
+        
+        XCTAssertEqual(packer.bytes, [ 0b00000000 ])
+    }
+
+    /// Add multiple single bit fields.
+    func testMultipleSingleBits() {
+        var packer = BitPacker()
+        packer.add(false)
+        packer.add(true)
+        
+        XCTAssertEqual(packer.bytes, [ 0b01000000 ])
+    }
+
+    /// Set a bit to true.
+    func testSetToTrue() {
+        var packer = BitPacker()
+        packer.add(true, at: 7)
         
         XCTAssertEqual(packer.bytes, [ 0b10000000 ])
     }
@@ -91,7 +151,7 @@ class BitPackerTests: XCTestCase {
     /// Set a bit to false.
     func testSetToFalse() {
         var packer = BitPacker()
-        packer.set(false, at: 7)
+        packer.add(false, at: 7)
         
         XCTAssertEqual(packer.bytes, [ 0b00000000 ])
     }
@@ -99,8 +159,8 @@ class BitPackerTests: XCTestCase {
     /// Setting a bit twice starts a new byte.
     func testSetSameBit() {
         var packer = BitPacker()
-        packer.set(true, at: 7)
-        packer.set(true, at: 7)
+        packer.add(true, at: 7)
+        packer.add(true, at: 7)
         
         XCTAssertEqual(packer.bytes, [ 0b10000000, 0b10000000 ])
     }
@@ -109,16 +169,17 @@ class BitPackerTests: XCTestCase {
     func testSetOverlapping() {
         var packer = BitPacker()
         packer.add(0b1010, at: 7, length: 4)
-        packer.set(true, at: 6)
+        packer.add(true, at: 6)
         
         XCTAssertEqual(packer.bytes, [ 0b10100000, 0b01000000 ])
     }
+    
     
     /// Signed values should still have all bits accessible.
     func testSignedValue() {
         let value: Int8 = -1
         var packer = BitPacker()
-        packer.add(value, at: 7, length: 8)
+        packer.add(value, length: 8)
         
         XCTAssertEqual(packer.bytes, [ 0b11111111 ])
     }
