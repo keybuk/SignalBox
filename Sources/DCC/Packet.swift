f79e45f511758c1f94b0bf10121726fbf61c7fcb
Scott James Remnant <scott@netsplit.com>
2018-05-19 19:02:52 -0700

    Forgot the packet start bit
    The WIP Packet code was missing the initial Packet Start Bit of 0.
    Put this in so it really does encapsulate the entire time in the packet
    .

--- Sources/DCC/Packet.swift
+++ Sources/DCC/Packet.swift
@@ -18,11 +18,12 @@ public extension Packet {
     public func add<T : Packer>(into packer: inout T) {
         let bytes = self.bytes
         for byte in bytes {
-            packer.add(byte, length: 8)
             packer.add(0, length: 1)
+            packer.add(byte, length: 8)
         }
         
         let errorDetectionByte = bytes.reduce(0, { $0 ^ $1 })
+        packer.add(0, length: 1)
         packer.add(errorDetectionByte, length: 8)
         
         packer.add(1, length: 1)
