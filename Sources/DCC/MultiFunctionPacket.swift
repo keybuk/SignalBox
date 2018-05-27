feaca9136d2ee9ab8b3d92c456c2bdb870952a12
Scott James Remnant <scott@netsplit.com>
2018-05-27 01:31:37 -0700

    Move the Preamble prototype into Packet.swift

--- Sources/DCC/MultiFunctionPacket.swift
+++ Sources/DCC/MultiFunctionPacket.swift
@@ -675,7 +675,7 @@ public struct ConfigurationVariableAccessInstruction : ConfigurationVariableInst
 
 }
 
-public struct ConfigurationVariableBitManipulateion : ConfigurationVariableInstruction {
+public struct ConfigurationVariableBitManipulation : ConfigurationVariableInstruction {
     
     public enum Operation {
         case verify
@@ -730,31 +730,3 @@ public struct ConfigurationVariableBitManipulateion : ConfigurationVariableInstr
     }
     
 }
-
-public struct Preamble : Packable {
-    
-    // FIXME: This is just a thought experiment, it might not be the best way to do preambles.
-    
-    public var timing: PulseTiming
-    public var withCutout: Bool
-    
-    init(timing: PulseTiming, withCutout: Bool = true) {
-        self.timing = timing
-        self.withCutout = withCutout
-    }
-    
-    public func add<T : Packer>(into packer: inout T) {
-        let count = withCutout ? timing.preambleCount : PulseTiming.preambleCountMin
-        if count <= UInt64.bitWidth {
-            // FIXME: since Packer iterates the bits anyway, is this really an optimisation?
-            let bits: UInt64 = ~(~0 << count)
-            packer.add(bits, length: count)
-        } else {
-            for _ in 0..<count {
-                packer.add(1, length: 1)
-            }
-        }
-    }
-    
-}
-
