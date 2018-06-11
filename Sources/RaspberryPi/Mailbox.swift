f0c3a3ce308ecb60b0c0e852fa1873106967799c
Scott James Remnant <scott@netsplit.com>
2018-06-10 21:27:54 -0700

    Copy over the Mailbox and UncachedMemory code
    Do this slightly differently, since we only ever intend to use Mailbox
    for this one thing, narrow its scope to be rather less sprawling.
    
    Then have UncachedMemory to be an RAII approach that completely wraps
    and hides it:
    
      let uncached = try UncachedMemory(minimumSize: 1024)
      // use uncached.pointer
    
      uncached.deallocate()
    
    Since we have to `try` anyway, just have that do the open of the Mailbox
    and clean itself up each time. there's not a huge benefit to keeping it
    open - same with the /dev/mem we open.

---
+++ Sources/RaspberryPi/Mailbox.swift
@@ -0,0 +1,210 @@
+//
+//  Mailbox.swift
+//  DCC
+//
+//  Created by Scott James Remnant on 12/21/16.
+//
+
+import Foundation
+
+/// Raspberry Pi Mailbox Property interface.
+///
+/// This class implements the Mailbox Property interface used to communicate to the VideoCore of
+/// the Raspberry Pi.
+///
+/// Only the memory allocation and locking properties are implemented at this time.
+///
+/// Documentation is available at https://github.com/raspberrypi/firmware/wiki/Mailbox-property-interface
+public final class Mailbox {
+    
+    /// Location of the `/dev/vcio` device.
+    private static let path = "/dev/vcio"
+    
+    /// File handle for `/dev/vcio`.
+    ///
+    /// We open this on initialization and retain it for the lifetime of the instance.
+    private let fileHandle: FileHandle
+    
+    /// - Throws: errors from `FileHandle(forReadingFrom:)`.
+    public init() throws {
+        fileHandle = try FileHandle(forReadingFrom: URL(fileURLWithPath: Mailbox.path))
+    }
+    
+    /// Error codes.
+    public enum Error : Swift.Error {
+    
+        case invalidRequest
+        case invalidResponse
+        case requestFailed
+        
+    }
+    
+    /// Values for property request's code member.
+    private static let processRequestCode: UInt32    = 0x00000000
+    private static let requestSuccessfulCode: UInt32 = 0x80000000
+    private static let parseErrorCode: UInt32        = 0x80000001
+    
+    /// Bit used to indicate values member was filled by response.
+    private static let responseValuesIndicator: UInt32 = 1 << 31
+    
+    /// Mailbox property ioctl.
+    ///
+    /// Expansion of `_IOWR(100, 0, char *)` from the Linux kernel headers.
+    private static let propertyIoctl = propertyIoctlDir | propertyIoctlSize | propertyIoctlType | propertyIoctlNr
+    private static let propertyIoctlDir: UInt = ((1 | 2) << 30)
+    private static let propertyIoctlSize: UInt = UInt(MemoryLayout<Int>.stride) << 16
+    private static let propertyIoctlType: UInt = 100 << 8
+    private static let propertyIoctlNr: UInt = 0 << 0
+
+    /// Property request tags that are currently implemented.
+    internal enum PropertyTag: UInt32 {
+        case allocateMemory = 0x3000c
+        case lockMemory     = 0x3000d
+        case unlockMemory   = 0x3000e
+        case releaseMemory  = 0x3000f
+    }
+
+    /// Make a property request.
+    ///
+    /// - Parameters:
+    ///   - tag: mailbox property tag to request.
+    ///   - values: request values for the tag.
+    ///
+    /// - Returns: array of response values.
+    ///
+    /// - Throws: `OSError` or `Error` on failure.
+    internal func propertyRequest(forTag tag: PropertyTag, values: [UInt32]) throws -> [UInt32] {
+        let bufferSize = 6 + values.count
+        var buffer: [UInt32] = Array(repeating: 0, count: bufferSize)
+        
+        buffer[0] = UInt32(MemoryLayout<UInt32>.stride * bufferSize)
+        buffer[1] = Mailbox.processRequestCode
+        buffer[2] = tag.rawValue
+        buffer[3] = UInt32(MemoryLayout<UInt32>.stride * values.count)
+        buffer[4] = UInt32(MemoryLayout<UInt32>.stride * values.count)
+        
+        buffer.replaceSubrange(5..<(5 + values.count), with: values)
+        buffer[5 + values.count] = 0
+        
+        let result = ioctl(fileHandle.fileDescriptor, Mailbox.propertyIoctl, UnsafeMutableRawPointer(mutating: buffer))
+        guard result == 0 else { throw OSError(errno: errno) }
+        
+        switch buffer[1] {
+        case Mailbox.requestSuccessfulCode:
+            guard buffer[4] & Mailbox.responseValuesIndicator != 0 else {
+                throw Error.invalidResponse
+            }
+            
+            let numberOfValues = Int(buffer[4] & ~Mailbox.responseValuesIndicator) / MemoryLayout<UInt32>.stride
+            return Array(buffer[5..<(5 + numberOfValues)])
+        case Mailbox.parseErrorCode:
+            throw Error.invalidRequest
+        default:
+            throw Error.invalidResponse
+        }
+    }
+    
+    /// Handle to memory on the GPU.
+    public typealias MemoryHandle = UInt32
+
+    /// Flags for `Mailbox.allocateMemory(size:alignment:flags)`
+    public struct AllocateMemoryFlags: OptionSet {
+        
+        public let rawValue: UInt32
+        
+        public init(rawValue: UInt32) {
+            self.rawValue = rawValue
+        }
+        
+        /// Can be resized to 0 at any time. Use for cached data.
+        public static let discardable   = AllocateMemoryFlags(rawValue: 1 << 0)
+        
+        /// Normal allocating alias. Don't use from ARM.
+        public static let normal        = AllocateMemoryFlags(rawValue: 0 << 2)
+        
+        /// 0xC alias.  Uncached.
+        public static let direct        = AllocateMemoryFlags(rawValue: 1 << 2)
+        
+        /// 0x8 alias.  Non-allocating in L2 but coherent.
+        public static let coherent      = AllocateMemoryFlags(rawValue: 2 << 2)
+        
+        /// Initialize buffer to all zeros.
+        public static let zero          = AllocateMemoryFlags(rawValue: 1 << 4)
+        
+        /// Don't initialize (default is to initialize to all ones.)
+        public static let noInit        = AllocateMemoryFlags(rawValue: 1 << 5)
+        
+        /// Likely to be locked for long periods of time.
+        public static let hintPermalock = AllocateMemoryFlags(rawValue: 1 << 6)
+        
+        /// Allocating in L2.
+        public static let l1Nonallocating: AllocateMemoryFlags = [ .direct, .coherent ]
+        
+    }
+    
+    /// Allocate contiguous memory on the GPU.
+    ///
+    /// Memory must be locked with `lockMemory(handle:)` before it can be accessed.
+    ///
+    /// The returned handle is not managed by the Linux kernel and will not be automatically freed
+    /// on program exit.
+    ///
+    /// - Parameters:
+    ///   - size: number of bytes to allocate.
+    ///   - alignment: alignment of returned allocation.
+    ///   - flags: `AllocateMemoryFlags`
+    ///
+    /// - Returns: `MemoryHandle` for allocated memory.
+    ///
+    /// - Throws: `OSError` or `Error` on failure.
+    public func allocateMemory(size: Int, alignment: Int, flags: AllocateMemoryFlags) throws -> MemoryHandle {
+        let response = try propertyRequest(forTag: .allocateMemory, values: [ UInt32(size), UInt32(alignment), flags.rawValue ])
+        guard response.count == 1 else { throw Error.invalidResponse }
+        
+        return response[0]
+    }
+    
+    /// Lock memory in place.
+    ///
+    /// The memory can be accessed through `mmap`.
+    ///
+    /// - Parameters:
+    ///   - handle: `MemoryHandle` to be locked.
+    ///
+    /// - Returns: bus address of memory.
+    ///
+    /// - Throws: `OSError` or `Error` on failure.
+    public func lockMemory(handle: MemoryHandle) throws -> UInt32 {
+        let response = try propertyRequest(forTag: .lockMemory, values: [ handle ])
+        guard response.count == 1 else { throw Error.invalidResponse }
+        
+        return response[0]
+    }
+    
+    /// Unlock memory.
+    ///
+    /// The memory will retain its contents, but may move.
+    ///
+    /// - Parameters:
+    ///   - handle: `MemoryHandle` to be unlocked.
+    ///
+    /// - Throws: `OSError` or `Error` on failure.
+    public func unlockMemory(handle: MemoryHandle) throws {
+        let response = try propertyRequest(forTag: .unlockMemory, values: [ handle ])
+        guard response.count == 1 else { throw Error.invalidResponse }
+        guard response[0] == 0 else { throw Error.requestFailed }
+    }
+    
+    /// Release memory.
+    ///
+    /// - Parameters:
+    ///   - handle: `MemoryHandle` to be released.
+    ///
+    /// - Throws: `OSError` or `Error` on failure.
+    public func releaseMemory(handle: MemoryHandle) throws {
+        let response = try propertyRequest(forTag: .releaseMemory, values: [ handle ])
+        guard response.count == 1 else { throw Error.invalidResponse }
+        guard response[0] == 0 else { throw Error.requestFailed }
+    }
+    
+}
---
+++ Sources/RaspberryPi/UncachedMemory.swift
@@ -0,0 +1,103 @@
+//
+//  UncachedMemory.swift
+//  RaspberryPi
+//
+//  Created by Scott James Remnant on 6/10/18.
+//
+
+#if os(Linux)
+import Glibc
+#else
+import Darwin
+#endif
+
+/// Region of Uncached Memory.
+///
+/// Uncached memory is accessed through the Raspberry Pi's "'C' Alias" such that all reads directly
+/// come from, and all writes directly go to, RAM bypassing the core's L1 and L2 caches. While such
+/// accesses are significantly slower, this allows for interacting with hardware such as the DMA
+/// Engine which cannot _see_ these caches.
+///
+/// Instances of this object must be retained as long as the region is required. The instance will
+/// release the memory region on deinitialization. In addition, due to the way in which uncached
+/// memory is allocated, the memory region is **not** automatically released on process exit.
+/// You must deallocate it manually by either releasing the object or calling `deallocate()`.
+///
+/// The region is accessed through the `pointer` member; accessing this after deallocation will
+/// trap.
+public final class UncachedMemory {
+
+    /// Mailbox handle for the memory region, used to release it.
+    private var handle: Mailbox.MemoryHandle?
+
+    /// Pointer to the region.
+    public let pointer: UnsafeMutableRawPointer
+
+    /// Bus address of the region.
+    ///
+    /// This address is within the "'C' Alias" and may be handed directly to hardware such as the
+    /// DMA Engine.
+    public let busAddress: UInt32
+
+    /// Size of the region
+    public let size: Int
+
+    /// - Parameters:
+    ///   - minimumSize: minimum size of region to allocate, will be rounded up to complete pages.
+    ///
+    /// - Throws: `Mailbox.Error` or `OSError` on failure.
+    public init(minimumSize: Int) throws {
+        size = Int(PAGE_SIZE) * (((minimumSize - 1) / Int(PAGE_SIZE)) + 1)
+
+        let memoryDevice = try MemoryDevice()
+        let mailbox = try Mailbox()
+
+        // From this point on, we have to remember to release the allocated memory on failure.
+        let handle = try mailbox.allocateMemory(size: size, alignment: Int(PAGE_SIZE), flags: .direct)
+        do {
+            busAddress = try mailbox.lockMemory(handle: handle)
+            let address = busAddress & ~RaspberryPi.uncachedAliasBusAddress
+
+            pointer = try memoryDevice.map(address: address, size: size)
+            self.handle = handle
+        } catch {
+            try! mailbox.releaseMemory(handle: handle)
+            throw error
+        }
+    }
+
+    /// Release the region.
+    ///
+    /// This is automatically called on object destruction, but may be called in advance if
+    /// necessary. Uncached memory regions must be deallocated manually on process exit, otherwise
+    /// the region will remain allocated.
+    ///
+    /// Accessing the memory region through `pointer` after calling this method will result in a
+    /// runtime error.
+    ///
+    /// - Throws: `Mailbox.Error` or `OSError` on failure.
+    public func deallocate() throws {
+        guard let handle = handle else { return }
+
+        // Release the memory with the mailbox first; since it matters more whether this takes place
+        /// than the munmap.
+        let mailbox = try Mailbox()
+        try mailbox.releaseMemory(handle: handle)
+        self.handle = nil
+
+        try MemoryDevice.unmap(pointer, size: size)
+    }
+
+    deinit {
+        // There isn't much we can do if we fail to release memory. Fortunately it's one of those
+        /// operations that also shouldn't ever happen.
+        do {
+            try deallocate()
+        } catch {
+            print("Unexpected error during memory release: \(error)")
+            print("!!! MEMORY MAY STILL BE ALLOCATED !!!")
+        }
+    }
+
+}
+
