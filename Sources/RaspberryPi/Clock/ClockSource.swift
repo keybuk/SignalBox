03dbe707238a45dd9dcd188176cc3fb52fa7b518
Scott James Remnant <scott@netsplit.com>
2018-06-10 17:31:15 -0700

    ClockSource - add the "known" frequency of clocks

--- Sources/RaspberryPi/Clock/ClockSource.swift
+++ Sources/RaspberryPi/Clock/ClockSource.swift
@@ -17,4 +17,15 @@ public enum ClockSource : UInt32 {
     case plld       = 6
     case hdmiAux    = 7
     
+    /// Clock's known frequency (in MHz).
+    public var frequency: Float {
+        switch self {
+        case .oscillator: return 19.2
+        // Note: .pllc is ommitted since its value is not stable.
+        case .plld: return 500
+        case .hdmiAux: return 216
+        default: return 0
+        }
+    }
+    
 }
