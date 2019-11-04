8b532e3a135dcdd68f927f1d914360d81fb313dc
Scott James Remnant <scott@netsplit.com>
2019-11-03 18:19:25 -0800

    Bump swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
