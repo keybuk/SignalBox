dd2159518ab0e5095c6933edf7606d269b3d7009
Scott James Remnant <scott@netsplit.com>
2019-11-16 21:54:22 -0800

    Add PacketPacker
    This takes the Packer pattern one step further by creating a packer that wraps another packer, passing through the calls, while also accumulating data itself.
    
    Before this `Packet` would need its own `BitPacker<UInt8>` to collect the values from the type-specific implementation, and the iterate those values against a different packer to insert the byte separation bits.
    
    Now the type-specific implementations can just use this instead without needing to collect the values as an intermediate step.

---
+++ Sources/DCC/PacketPacker.swift
@@ -0,0 +1,76 @@
+//
+//  PacketPacker.swift
+//  DCC
+//
+//  Created by Scott James Remnant on 11/6/19.
+//
+
+import Foundation
+
+/// Pass-through `Packer` for DCC packets.
+///
+/// `PacketPacker` passes values added to it through another `Packer` while inserting byte separation bits and accumulatiing
+/// the error detection byte.
+///
+/// Values added should conform to the more strict structure of DCC address and instruction bytes, resulting in exact multiples of
+/// eight bits.
+///
+/// Additionally to add the accumulated error detection byte and return the internal copy of the wrapped `Packer`, `finish`
+/// must be called.
+///
+/// # Example:
+///      var signalPacker = SignalPacker(timing: SignalTiming(pulseWidth: 14.5))
+///      var packer = PacketPacker(packer: signalPacker)
+///      packer.add(0b10101100, 8)
+///      signalPacker = packer.finish()
+public struct PacketPacker<SubPacker> : Packer
+where SubPacker : Packer {
+    /// Wrapped `Packer` copy.
+    public var packer: SubPacker
+
+    /// Number of bits remaining in the current byte.
+    private var bitsRemaining = 0
+
+    /// Accumulated error detection byte.
+    private var errorDetectionByte: UInt8 = 0
+
+    public init(packer: SubPacker) {
+        self.packer = packer
+    }
+
+    public mutating func add<T>(_ value: T, length: Int) where T : FixedWidthInteger {
+        precondition(length > 0, "length must be greater than 0")
+        precondition(length <= T.bitWidth, "length must be less than or equal to \(T.bitWidth)")
+
+        var length = length
+        repeat {
+            if bitsRemaining == 0 {
+                bitsRemaining = 8
+                packer.add(0b0, length: 1)
+            }
+
+            let chunkLength = min(bitsRemaining, length)
+            bitsRemaining -= chunkLength
+            length -= chunkLength
+
+            packer.add(value >> length, length: chunkLength)
+
+            let bits = UInt8(truncatingIfNeeded: value >> length) & UInt8.mask(bits: chunkLength)
+            errorDetectionByte ^= bits << bitsRemaining
+        } while length > 0
+    }
+
+    /// Returns the internal copy of the wrapped `Packer`.
+    ///
+    /// Adds the error detection byte and packet end-bit, and then returns the internal copy of `packer` which has had the
+    /// called `add(:length:)` methods called on it.
+    public mutating func finish() -> SubPacker {
+        precondition(bitsRemaining == 0, "Packet must consist of whole bytes")
+
+        packer.add(0b0, length: 1)
+        packer.add(errorDetectionByte)
+        packer.add(0b1, length: 1)
+
+        return packer
+    }
+}
---
+++ Tests/DCCTests/PacketPackerTests.swift
@@ -0,0 +1,44 @@
+//
+//  PacketPackerTests.swift
+//  DCCTests
+//
+//  Created by Scott James Remnant on 11/16/19.
+//
+
+import XCTest
+
+import DCC
+
+class PacketPackerTests: XCTestCase {
+
+    /// Test that we can add a single byte.
+    func testSingleByte() {
+        var packer = PacketPacker(packer: BitPacker<UInt32>())
+        packer.add(0b10101100, length: 8)
+
+        let subpacker = packer.finish()
+        XCTAssertEqual(subpacker.results, [0b0_10101100_0_10101100_1 << subpacker.bitsRemaining])
+    }
+
+    /// Test that we can add two bytes, and the error detection bit is an XOR of both.
+    func testTwoBytes() {
+        var packer = PacketPacker(packer: BitPacker<UInt32>())
+        packer.add(0b10101100, length: 8)
+        packer.add(0b01010110, length: 8)
+
+        let subpacker = packer.finish()
+        XCTAssertEqual(subpacker.results, [0b0_10101100_0_01010110_0_11111010_1 << subpacker.bitsRemaining])
+    }
+
+    /// Test that we can add values across the byte boundary, and the byte separator bit is inserted between.
+    func testValueAcrossByte() {
+        var packer = PacketPacker(packer: BitPacker<UInt32>())
+        packer.add(0b1010, length: 4)
+        packer.add(0b11000101, length: 8)
+        packer.add(0b0110, length: 4)
+
+        let subpacker = packer.finish()
+        XCTAssertEqual(subpacker.results, [0b0_10101100_0_01010110_0_11111010_1 << subpacker.bitsRemaining])
+    }
+
+}
