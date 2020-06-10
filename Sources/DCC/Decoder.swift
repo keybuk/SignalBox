98d4ad1296814ef3fce668e064597f6d2c5e74cd
Scott James Remnant <scott@netsplit.com>
2020-06-10 10:14:52 -0700

    Stub Decoder and Dispatcher

---
+++ Sources/DCC/Decoder.swift
@@ -0,0 +1,13 @@
+//
+//  Decoder.swift
+//  DCC
+//
+//  Created by Scott James Remnant on 6/9/20.
+//
+
+import Foundation
+
+public struct Decoder {
+    var address: Address
+    var speed: Int
+}
---
+++ Sources/DCC/Dispatcher.swift
@@ -0,0 +1,12 @@
+//
+//  Dispatcher.swift
+//  DCC
+//
+//  Created by Scott James Remnant on 6/9/20.
+//
+
+import Foundation
+
+public struct Dispatcher {
+    var decoders: [Address: Decoder] = [:]
+}
